a19e0711c78e9de3ca5b1fbe96b6bb02925fd3c4
benja <benjamin.aponte.2030@gmail.com>
2025-09-23 18:51:27 +0100

    Cambios javascript

--- js/tipos.js
+++ js/tipos.js
@@ -18,3 +18,15 @@ let b = 'XXX';
 let resultado = a / b;
 console.log(typeof a);
 
+let seleccion = 459;
+
+let productos = [
+    {id: 123, nombre: "Pantalon", precio: 25, imagen: 'pantalon.png'},
+    {id: 459, nombre: "Camisa", precio: 15, imagen: 'camisa.png'},
+    {id: 560, nombre: "Zapatos", precio: 45, imagen: 'zapatos.png'},
+    {id: 789, nombre: "Gorra", precio: 5, imagen: 'gorra.png'}
+]
+
+for(let i=0; i<productos.length; i++){
+    console.log(productos[i]);
+}
