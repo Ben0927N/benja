e7a1b038976a812d7ee940917ed2639eff06c9a0
benja <benjamin.aponte.2030@gmail.com>
2025-10-03 10:00:48 +0100

    pokemon

---
+++ DOR/pokemon_API/app.js
@@ -0,0 +1,7 @@
+async function getpokemon(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await response.json()
+    return data
+}
+
+console.log(getpokemon(183))
